02bc9f097ce7e9e2e2076b2babc4183c2ad57aff
Suraj Pisal <surajpisal113@gmail.com>
2022-12-27 17:09:11 +0000

    added CSRF_TRUSTED_ORIGIN to avoid csrf token error

--- mysite/settings.py
+++ mysite/settings.py
@@ -168,3 +168,6 @@ GOOGLE_RECAPTCHA_SITE_KEY = env('GOOGLE_RECAPTCHA_SITE_KEY')
 GOOGLE_RECAPTCHA_SECRET_KEY = env('GOOGLE_RECAPTCHA_SECRET_KEY')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# avoid csrf token error
+CSRF_TRUSTED_ORIGINS = ['https://heart-charity.up.railway.app/']
