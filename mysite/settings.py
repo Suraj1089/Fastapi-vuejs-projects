40d43569ca3b0b53c33a0653c6e33da09dcfd03d
Suraj Pisal <surajpisal113@gmail.com>
2022-12-28 16:12:59 +0530

    added vercel requirements

--- mysite/settings.py
+++ mysite/settings.py
@@ -141,8 +141,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'statcfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'mysite/static')
 ]
