fad930e9d249261d8557c2b291cf2f34bc1761c1
Suraj Pisal <surajpisal113@gmail.com>
2023-01-12 00:17:36 +0530

    added vue js

--- base/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('contact/', views.contact, name="contact"),
-    path('test/', views.test, name="test"),
-    path('volunteer/', views.voluteer_form, name="volunteer"),  
-]
