adac54400bfdd0f0efa18088c16d66ffba8240fc
Suraj Pisal <surajpisal113@gmail.com>
2023-01-15 11:46:38 +0530

    added nofication to navbar

--- frontend/charity/src/components/NavBar.vue
+++ frontend/charity/src/components/NavBar.vue
@@ -8,6 +8,11 @@
           <!-- user image -->
          <li><img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" class="user-logo" v-on:click="isProfileOpen=true"></li>
       </ul>
+      <!--notification-->
+      <div class="notification">
+          <h3 class="notification-content"><span>{{ notificationContent }}</span></h3>
+      </div>
+          
   </div>
 </template>
 
@@ -17,7 +22,8 @@ export default {
   name: 'NavBar',
   data() {
     return {
-      isProfileOpen: false
+      isProfileOpen: true,
+      notificationContent: "Sorry for inconvenicence but we are currently under development"
     }
   },
   methods: {
@@ -31,6 +37,19 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
+.notification-content {
+  color: rgb(0, 0, 0);
+  text-align: center;
+  background-color: #e5d3d3;
+  justify-content: center;
+  align-content: center;
+  height: 50px;
+  margin-top: 10px;
+  margin-right: 200px;
+  margin-left: 200px;
+  border-radius: 5px;
+
+}
 .user-logo {
   border-radius: 50%;
   width: 40px;
